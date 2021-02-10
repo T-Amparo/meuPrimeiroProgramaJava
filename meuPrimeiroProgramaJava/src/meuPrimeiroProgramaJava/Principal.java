5d8d8f1669d4f30324e2d1e7638c5e51c2748a04
T-Amparo <78854010+T-Amparo@users.noreply.github.com>
2021-02-10 09:51:06 -0300

    Update Principal.java
    Nota: Implementar sobrenome.

--- meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
+++ meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
@@ -1,8 +1,8 @@
 /**
  * Este programa mostra ao aluno:
- * 1) Que um programa Java é construido dentro de um projeto Java.
- * 2) Todo programa Java deve ter uma classe com um método main.
- * 3) Como exibir informações no terminal (console) do Java.
+ * 1) Que um programa Java Ã© construido dentro de um projeto Java.
+ * 2) Todo programa Java deve ter uma classe com um mÃ©todo main.
+ * 3) Como exibir informaÃ§Ãµes no terminal (console) do Java.
  * 4) Como ler um String (console) do Java.
  * 
  */
@@ -14,25 +14,27 @@ import java.util.Scanner;
  * Data: 03/02/2021
  *
  */
-public class Principal { // Classe que cont�m o metodo principal.
+public class Principal { // Classe que contém o metodo principal.
 	public static void main(String[] args) {
 		
-		// Salda��o e pergunta o nome.
+		// Saldação e pergunta o nome.
 		System.out.println("Hello Word!\nQual o seu nome?"); // Uso do println
 		
 		// Ler o nome.		
 		Scanner in = new Scanner(System.in);
 		String nome = in.nextLine();
 		
-		// Salda��o especifica para o nome lido.		
-		System.out.printf("Ol� %s!\n", nome);			
+		// Saldação especifica para o nome lido.		
+		System.out.printf("Olá %s!\n", nome);			
 		
 		// Perguntar Idade
-		System.out.printf("%s, Qual � a sua idade?", nome);
+		System.out.printf("%s, Qual é a sua idade?", nome);
 		
 		//Ler Idade
 		Integer idade = in.nextInt();
-		System.out.printf("%d! Puxa! Como voc� � jovem!", idade);
+		System.out.printf("%d! Puxa! Como você é jovem!", idade);
+		
+		// Nota: Implementar sobrenome.
 	}
 
 }
